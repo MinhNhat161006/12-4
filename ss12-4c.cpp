f1a530b20c2847b504adb0fce66cb0759ce48303
MinhNhat161006 <n24dtcn060@student.ptithcm.edu.vn>
2024-12-27 17:23:58 +0700

    Add files via upload

---
+++ ss12-4c.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int timSoLonNhat(int arr[], int size) {
+    int max = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int n;
+    printf("Nhập số lượng phần tử của mảng: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Nhập các phần tử của mảng:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = timSoLonNhat(arr, n);
+    printf("Số lớn nhất trong mảng là: %d\n", max);
+
+    return 0;
+}
